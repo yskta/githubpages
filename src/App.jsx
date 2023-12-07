5401bb2cd38a7745d0a63cd9044248638c2a3aab
Yoshifumi Kitagawa <kitapo3110@gmail.com>
2023-12-08 00:24:41 +0900

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './home';
 import Sidebar from './sidebar';
 import Profile from './profile';
